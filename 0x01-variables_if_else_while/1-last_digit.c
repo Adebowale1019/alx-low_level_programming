18cf1a58c3004ea4d0677ff1716b00eaaba3f747
Adebowale1019 <Adesokanadebowhale@gmail.com>
2023-08-13 14:05:39 -0700

    update

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,32 +1,26 @@
-#include <stdlib.h> 
- #include <time.h> 
- #include <stdio.h> 
+#include <stdlib.h>
+#include <time.h> 
+#include <stdio.h> 
   
  /** 
-  * main - Prints a text according number 
+  * main - Entry point
+  * Date : 8/13/2023 10:02pm
+  * details:printing last digit number.
   * 
   * Return: Always (Success) 
   */ 
  int main(void) 
  { 
-         int n, lastd; 
-  
-         srand(time(0)); 
-         n = rand() - RAND_MAX / 2; 
-         lastd = n % 10; 
-  
-         if (lastd > 5) 
-         { 
-                 printf("Last digit of %d is %d and is greater than 5\n", n, lastd); 
-         } 
-         else if (lastd == 0) 
-         { 
-                 printf("Last digit of %d is %d and is 0\n", n, lastd); 
-         } 
-         else if (lastd < 6 && lastd != 0) 
-         { 
-                 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastd); 
-         } 
-  
+       int n = lastd;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 5)
+		printtf("if the %d of %d is greater than 5: the string %d is greater than 5\n", lastd);
+	else if (n == 0)
+		printf("if the %d of %d is 0: the string %d is 0\n", lastd);
+	else if (n < 6 && n == 0)
+		printf("if the %d of %d is less than 6 and not 0: the string %d is less than 6 and not 0\n", lastd);
+
          return (0); 
  }
