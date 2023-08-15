305e0a560f669e1f5121c774add6661c3d91ea83
Adebowale1019 <Adesokanadebowhale@gmail.com>
2023-08-14 21:19:42 -0700

    update

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point 
+ * author - Adebowale
+ * date - 8/15/2023 5:04am
+ * return - (sucess) always
+ **/
+char name[100];
+char name[] = Adebowale;
+{
+putchar("%s\n", Adebowale);
+putchar("\n");
+
+return(0);
+}
+
