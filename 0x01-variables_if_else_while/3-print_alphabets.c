784378c94edff8d69c521c1224f1fd00cd8e7d7b
Adebowale1019 <Adesokanadebowhale@gmail.com>
2023-08-14 22:15:55 -0700

    updated

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,10 +9,10 @@ int main(void)
 {
 	char c;
 	char d;
-	for (c = 'a'; c <= 'z'; c++);
-	putchar(c);
-	for (d = 'A'; d <= 'Z'; d++);
-	putchar (d);
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	for (d = 'A'; d <= 'Z'; d++)
+		putchar (d);
 	
 	return (0);
 }
