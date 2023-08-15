52b9dfcb9ddbe3b5614f37a9e41e10a4f967a116
Adebowale1019 <Adesokanadebowhale@gmail.com>
2023-08-14 17:58:39 -0700

    updated

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,4 +1,4 @@
-#include <stdlib.h>
+e#include <stdlib.h>
 #include <time.h> 
 #include <stdio.h> 
   
@@ -12,6 +12,7 @@
  int main(void) 
  { 
        int n,lastd;
+       int lastd => 10;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
