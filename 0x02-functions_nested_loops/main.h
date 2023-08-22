8de5065d58f06644b8842c129d2ca5e747e0ba98
Adebowale1019 <Adesokanadebowhale@gmail.com>
2023-08-22 07:42:02 -0700

    updated

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,16 +1,23 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include "main.h"
 
-void _putchar(char c);
-void reset_to_98(int *n);
-void swap_int(int *a, int *b);
-int _strlen(char *s);
-void _puts(char *str);
-void print_rev(char *s);
-void rev_string(char *s);
-void puts2(char *str);
-void puts_half(char *str);
-void print_array(int *a, int n);
-char *_strcpy(char *dest, char *src);
+/**
+*main - Entry point
+*
+*Return: nothing, return void
+*/
 
-#endif
+int main(void)
+
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
