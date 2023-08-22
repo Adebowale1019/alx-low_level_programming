51b920d42dde109c8312cd27aab22f6be781f131
Adebowale1019 <Adesokanadebowhale@gmail.com>
2023-08-22 01:19:54 -0700

    keygen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -3,22 +3,26 @@
 #include <time.h>
 
 /**
- *main - generates random valid passwords
- *Return: 0 (on success).
+ * main - entry point generates random password
+ *
+ * Return: the generated password
  */
+
 int main(void)
 {
-	int pass, sum;
+char c;
+int x;
 
-	srand(time(NULL));
-	sum = 0;
-	while (sum <= 2645)
-	{
-		pass = (rand() % 128);
-		sum += pass;
-		printf("%c", pass);
-	}
-	printf("%c", 2772 - sum);
+srand(time(0));
 
-	return (0);
+while (x <= 2645)
+{
+c = rand() % 128;
+x += c;
+putchar(c);
+}
+
+putchar(2772 - x);
+
+return (0);
 }
