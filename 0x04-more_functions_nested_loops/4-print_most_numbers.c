ab34e579c108d7e351d22f737b13abee30558156
Adebowale1019 <Adesokanadebowhale@gmail.com>
2023-08-26 09:26:42 -0700

    print_most_numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints number
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+ for (i = 0; i < 9; i++)
+ {
+	 if (i != 2 && i != 4)
+	 {
+		 _putchar(0);
+	 }
+	 _putchar('\n');
+ 
+ }
