db565aa5f60b739c2c675302557dce47b6703f1d
Adebowale1019 <Adesokanadebowhale@gmail.com>
2023-08-26 10:12:38 -0700

    print_most_numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,16 +1,18 @@
 #include "main.h"
 /**
  * print_most_numbers - prints number
- */
+ **/
+
 void print_most_numbers(void)
 {
 	int i;
+
 	for (i = 0; i < 9; i++)
- {
-	 if (i != 2 && i != 4)
-	 {
-		 _putchar(i);
-	 }
-	 _putchar('\n');
- }
- }
+	{
+		if (i != 2 && i != 4)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+}
